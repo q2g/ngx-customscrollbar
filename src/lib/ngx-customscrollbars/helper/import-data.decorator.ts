7771d1782c86eab6c7e03d7395f14f4e3c05a997
Ralf Hannuschka <ralf.hannuschka@akquinet.de>
2019-06-14 19:08:57 +0200

    fix(lib): model data not set correctly [closes#122]

--- src/lib/ngx-customscrollbars/helper/import-data.decorator.ts
+++ src/lib/ngx-customscrollbars/helper/import-data.decorator.ts
@@ -8,11 +8,8 @@ export function importData<T>(target, key = null, descriptor: PropertyDescriptor
     const originalMethod = descriptor.value;
     descriptor.value = function (data: T) {
         // get all properties, exclude decorated property
-        const properties = Object.keys(target).filter((prop) => prop !== key);
-        properties.forEach((property) => {
-            if (data.hasOwnProperty(property)) {
-                this[property] = data[property];
-            }
+        Object.keys(data).forEach((property) => {
+            this[property] = data[property];
         });
         // call original descriptor
         const result = originalMethod.call(this, data);
