19a6c0eed774434ded9e3f466d75b7e62889a901
thomashaenig <thomas.haenig@akquinet.de>
2022-05-11 15:29:00 +0200

    update in progress

--- src/lib/ngx-customscrollbars/custom-scrollbars.module.ts
+++ src/lib/ngx-customscrollbars/custom-scrollbars.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import {NgxCustomScrollbarComponent} from "./components/scrollbar.component";
-import {NgxCustomScrollbarHideOnNativeStyleDirective } from "./directives/no-native-style.directive";
-import {NgxCustomScrollbarScrollableDirective} from "./directives/scrollable-container.directive";
-import {NgxCustomScrollbarOverflowYDirective} from "./directives/overflow-y.directive";
-import {NgxCustomScrollbarOverflowXDirective} from "./directives/overflow-x.directive";
+import { NgxCustomScrollbarComponent} from "./components/scrollbar.component";
+import { NgxCustomScrollbarHideOnNativeStyleDirective } from "./directives/no-native-style.directive";
+import { NgxCustomScrollbarScrollableDirective } from "./directives/scrollable-container.directive";
+import { NgxCustomScrollbarOverflowYDirective } from "./directives/overflow-y.directive";
+import { NgxCustomScrollbarOverflowXDirective } from "./directives/overflow-x.directive";
 import { ViewportControl } from "./provider/viewport.control";
 
 @NgModule({
--- src/lib/public_api.ts
+++ src/lib/public_api.ts
@@ -7,3 +7,8 @@ export * from "./ngx-customscrollbars/model/overflow";
 export * from "./ngx-customscrollbars/provider/viewport.control";
 export * from "./ngx-customscrollbars/provider/window-resize";
 export * from "./ngx-customscrollbars/viewport/viewport";
+export * from "./ngx-customscrollbars/directives/scrollable-container.directive"
+export * from "./ngx-customscrollbars/directives/overflow-x.directive"
+export * from "./ngx-customscrollbars/directives/overflow-y.directive"
+export * from "./ngx-customscrollbars/directives/no-native-style.directive"
+export * from "./ngx-customscrollbars/components/scrollbar.component"
