c684b80d8c4bd3322ccd65a3fd7108a5a6d82dfc
Christian Rimpau <Christian.Rimpau@akquinet.de>
2025-06-26 14:56:49 +0200

    run "ng update @angular/core@15 @angular/cli@15"

--- src/lib/test.ts
+++ src/lib/test.ts
@@ -4,21 +4,13 @@ import "core-js/es7/reflect";
 import "zone.js";
 import "zone.js/testing";
 import { getTestBed } from "@angular/core/testing";
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
-} from "@angular/platform-browser-dynamic/testing";
-
-declare const require: any;
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from "@angular/platform-browser-dynamic/testing";
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(), {
-    teardown: { destroyAfterEach: false }
-}
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting(),
+    {
+        teardown: { destroyAfterEach: false }
+    }
 );
-// Then we find all the tests.
-const context = require.context("./", true, /\.spec\.ts$/);
-// And load the modules.
-context.keys().map(context);
