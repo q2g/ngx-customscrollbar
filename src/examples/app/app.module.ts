57fc84e24acfb160be466ec0e1e47bce399e670c
Christian Rimpau <Christian.Rimpau@akquinet.de>
2025-06-26 15:03:29 +0200

    run "ng update @angular/material@15"

--- src/examples/app/app.module.ts
+++ src/examples/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
-import { MatTableModule } from "@angular/material/table";
+import { MatLegacyTableModule as MatTableModule } from "@angular/material/legacy-table";
 
 import { AppComponent } from "./app.component";
 import { VerticalScrollComponent } from "./vertical-scroll/vertical-scroll.component";
@@ -14,48 +14,48 @@ import { CdkVirtualScrollOverviewExampleComponent } from "./virtual-scroll/virtu
 import { ScrollingModule } from "@angular/cdk/scrolling";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AsyncScrollComponent,
-    HorizontalScrollComponent,
-    HorizontalVerticalScrollComponent,
-    VerticalScrollComponent,
-    CdkVirtualScrollOverviewExampleComponent,
-    TableComponent
-  ],
-  imports: [
-    ScrollingModule,
-    MatTableModule,
-    NgxCustomScrollbarModule,
-    BrowserModule,
-    RouterModule.forRoot([
-        {
-            path: "",
-            component: VerticalScrollComponent
-        },
-        {
-            path: "virtual",
-            component: CdkVirtualScrollOverviewExampleComponent
-        },
-        {
-            path: "horizontal",
-            component: HorizontalScrollComponent
-        },
-        {
-            path: "horizontal-vertical",
-            component: HorizontalVerticalScrollComponent
-        },
-        {
-            path: "async",
-            component: AsyncScrollComponent
-        },
-        {
-            path: "table",
-            component: TableComponent
-        }
-    ])
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        AsyncScrollComponent,
+        HorizontalScrollComponent,
+        HorizontalVerticalScrollComponent,
+        VerticalScrollComponent,
+        CdkVirtualScrollOverviewExampleComponent,
+        TableComponent
+    ],
+    imports: [
+        ScrollingModule,
+        MatTableModule,
+        NgxCustomScrollbarModule,
+        BrowserModule,
+        RouterModule.forRoot([
+            {
+                path: "",
+                component: VerticalScrollComponent
+            },
+            {
+                path: "virtual",
+                component: CdkVirtualScrollOverviewExampleComponent
+            },
+            {
+                path: "horizontal",
+                component: HorizontalScrollComponent
+            },
+            {
+                path: "horizontal-vertical",
+                component: HorizontalVerticalScrollComponent
+            },
+            {
+                path: "async",
+                component: AsyncScrollComponent
+            },
+            {
+                path: "table",
+                component: TableComponent
+            }
+        ])
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
