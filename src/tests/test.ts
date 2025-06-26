35bc43354d593edd266e3741f98bf114dcf64b15
Christian Rimpau <Christian.Rimpau@akquinet.de>
2025-06-26 16:02:12 +0200

    auto format

--- src/tests/test.ts
+++ src/tests/test.ts
@@ -4,19 +4,17 @@ import "core-js/es7/reflect";
 import "zone.js";
 import "zone.js/testing";
 import { getTestBed } from "@angular/core/testing";
-import {
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
-} from "@angular/platform-browser-dynamic/testing";
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from "@angular/platform-browser-dynamic/testing";
 
 declare const require: any;
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
-  BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(), {
-    teardown: { destroyAfterEach: false }
-}
+    BrowserDynamicTestingModule,
+    platformBrowserDynamicTesting(),
+    {
+        teardown: { destroyAfterEach: false }
+    }
 );
 // Then we find all the tests.
 const context = require.context("./", true, /\.spec\.ts$/);
